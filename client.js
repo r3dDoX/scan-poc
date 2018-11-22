c6fb58a6445840baea71c9f6f71d5bbdb9f19fcd
Patrick Walther <patrick.walther@visana.ch>
2018-11-22 14:39:27 +0100

    scale canvas

--- client.js
+++ client.js
@@ -12,12 +12,13 @@ canvas.height = 360;
 button.addEventListener('click', () => {
     video.classList.remove('fullscreen');
     video.classList.add('hidden');
-    button.classList.add('hidden');
     canvas.classList.add('fullscreen');
     canvas.classList.remove('hidden');
 
+    const ratio = Math.max(window.devicePixelRatio || 1, 1);
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
+    canvas.getContext('2d').scale(ratio, ratio);
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 });
 
