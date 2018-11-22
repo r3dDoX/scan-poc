2087ac0e0397863371502b0b4647b271d89ff085
Patrick Walther <patrick.walther@visana.ch>
2018-11-22 16:57:56 +0100

    render image

--- client.js
+++ client.js
@@ -3,10 +3,18 @@ const canvas = document.querySelector('canvas');
 
 input.addEventListener('change', handleFiles, false);
 
+window.onload = () => {
+    const ratio = Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext('2d').scale(ratio, ratio);
+};
+
 function handleFiles(e) {
     var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     var url = URL.createObjectURL(e.target.files[0]);
     var img = new Image();
-    img.onload = () => ctx.drawImage(img, 20, 20);
+    img.onload = () => ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.offsetWidth, img.height * (canvas.offsetWidth / img.width))
     img.src = url;
 }
