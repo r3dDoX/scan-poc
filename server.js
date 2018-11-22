d23a6805131aa45339afc1166237cbe326f07f11
Patrick Walther <patrick.walther@visana.ch>
2018-11-22 14:29:45 +0100

    disable caching

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const httpServer = require('http-server');
 
-const server = httpServer.createServer();
+const server = httpServer.createServer({ cache: 0 });
 server.listen({port: process.env.PORT || 3000}, () => console.log('started'));
